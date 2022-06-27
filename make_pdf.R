fcc80c7559968b0b161bebd930c9d403fbd17262
Paolo Bosetti <paolo.bosetti@unitn.it>
2022-06-27 10:32:38 +0200

    Added make_pdf script

---
+++ make_pdf.R
@@ -0,0 +1,32 @@
+#!/usr/local/bin/Rscript
+#Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+library(stringr)
+
+files <- c(
+  "0-Introduzione_R.Rmd",
+  "1-StatisticaDescrittiva.Rmd",
+  "2-Tidyverse.Rmd",
+  "3-StatisticaInferenziale.Rmd",
+  "4-Regressione.Rmd",
+  "5-Bootstrapping.Rmd",
+  "6-MisuraIncertezzaTaratura.Rmd",
+  "7-DesignOfExperiments.Rmd"
+)
+out_files <- c()
+
+for (f in files) {
+  cat(paste("********* Building source", f, "**********\n"))
+  out_files <- c(out_files, str_replace(f, "\\.Rmd$", ".pdf"))
+  rmarkdown::render(
+    input = f,
+    quiet = T,
+    clean = T
+  )
+}
+
+# merge all files
+gs_args <- c("-q", "-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite", "-sOutputFile=merged.pdf", out_files)
+system2("gs", gs_args)
+
+# cleanup
+system2("rm", c("*.log"))
